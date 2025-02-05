bc66fa26d21d37486bb826f64bc5bf45e7af8fb2
swizal <swizaljanice100@gmail.com>
2025-02-05 14:15:23 +0530

    Changes in routes

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -3,15 +3,16 @@ import connectDB from "@/lib/mongodb";
 import Task from "@/models/Task";
 
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+export async function PUT(req: Request, context: { params: { id: string } }) {
   await connectDB();
   
- 
+
   const { completed } = await req.json();
   
-  const updatedTask = await Task.findByIdAndUpdate(params.id, { completed }, { new: true });
-
  
+  const updatedTask = await Task.findByIdAndUpdate(context.params.id, { completed }, { new: true });
+
+
   if (!updatedTask) {
     return NextResponse.json({ message: "Task not found" }, { status: 404 });
   }
@@ -19,9 +20,13 @@ export async function PUT(req: Request, { params }: { params: { id: string } }) 
   return NextResponse.json({ message: "Task updated successfully", task: updatedTask });
 }
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+
+export async function DELETE(req: Request, context: { params: { id: string } }) {
   await connectDB();
-  const deletedTask = await Task.findByIdAndDelete(params.id);
+  
+
+  const deletedTask = await Task.findByIdAndDelete(context.params.id);
+
   if (!deletedTask) {
     return NextResponse.json({ message: "Task not found" }, { status: 404 });
   }
